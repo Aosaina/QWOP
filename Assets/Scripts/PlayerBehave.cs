3e2dfade7e50b699ab4039770b5a75ee3eb8a671
Lucy Zhai <zz3635@nyu.edu>
2023-02-08 12:55:46 -0500

    player code

--- Assets/Scripts/PlayerBehave.cs
+++ Assets/Scripts/PlayerBehave.cs
@@ -4,10 +4,16 @@ using UnityEngine;
 
 public class PlayerBehave : MonoBehaviour
 {
+    public Rigidbody2D armBoby;
+    Rigidbody2D mainBoby;
+
+    public float power;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+     mainBoby = GetComponent<Rigidbody2D>();
+
     }
 
     // Update is called once per frame
